ba2f0b72c6d7b333ca98460259b9a871c665934c
Henry Pan <henrypan917@gmail.com>
2016-01-07 15:44:28 -0500

    corrected solution to address issue when submitting a form with empty field

--- events/forms.py
+++ events/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class InformationForm(forms.Form):
-    method = forms.CharField(label='Method', max_length=30)
-    note = forms.CharField(label='Note', max_length=200)
+    method = forms.CharField(label='Method', max_length=30, required=False)
+    note = forms.CharField(label='Note', max_length=200, required=False)
